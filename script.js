63b03bb3962abb21b1b1e8103c6ce2e032c7b6a0
sum1t_here <mazumdarsumit3@gmail.com>
2023-05-23 14:24:40 +0530

    mouseout

---
+++ script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box");
+
+// add a mouseout event listener to the element
+boxElement.addEventListener("mouseout", () => {
+  alert("mouse out event working properly");
+});
